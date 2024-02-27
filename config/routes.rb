8bc65e3f09dceb1ad9911bb346237933921fd9c6
Riokyanpu <kgst1216@gmail.com>
2024-02-27 14:23:05 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-
+  resources :rooms, only: [:new, :create]
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
- end
+
